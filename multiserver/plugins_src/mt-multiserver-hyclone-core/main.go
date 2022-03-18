7aaf6bf06137f771cce18d78856e2ac692f86def
AFCMS <afcm.contact@gmail.com>
2022-03-18 18:50:20 +0100

    quick fix

--- multiserver/plugins_src/mt-multiserver-hyclone-core/main.go
+++ multiserver/plugins_src/mt-multiserver-hyclone-core/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	proxy "github.com/HimbeerserverDE/mt-multiserver-proxy"
 )
 
 func init() {
 	fmt.Println("Hello!")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, fmt.Sprint(proxy.Players()))
-    })
-    http.ListenAndServe(":5050", nil)
+    })*/
+    //http.ListenAndServe(":5050", nil)
 }
